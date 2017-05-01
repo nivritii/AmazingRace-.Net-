bc95f08cd88c86a3cc852a182f2c6c7b7f1fe86d
RachelGnanaraj <e0146843@u.nus.edu>
2017-05-01 21:30:17 +0800

    Dynamic path and python simulator converted to exe

--- ACE/ACE/Controllers/HomeController.cs
+++ ACE/ACE/Controllers/HomeController.cs
@@ -53,14 +53,14 @@ namespace ACE.Controllers
 
         public ActionResult simulateEvent()
         {
-                ProcessStartInfo start = new ProcessStartInfo();
-                start.FileName = "C:/Python27/python.exe";
-                start.Arguments = string.Format("{0} {1}", "C:/Users/Rach/Desktop/pythonSimulator.py", null);
-                start.UseShellExecute = false;
-                start.RedirectStandardOutput = true;
-                using (Process process = Process.Start(start))
-                {
-                    using (StreamReader reader = process.StandardOutput)
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = Path.Combine(Server.MapPath("."), "..\\..\\pythonSimulator.exe");
+            start.Arguments = string.Format("{0} {1}", "localhost 62061", null);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
                     {
                         string result = reader.ReadToEnd();
                         Console.Write(result);
